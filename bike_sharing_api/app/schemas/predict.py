0d0b9a281198413928b17f39ac092842e0e2f4b8
Chandra Sekar A <chandrasekar.a.1697@gmail.com>
2025-03-15 01:06:32 +0530

    added FastAPI code

---
+++ bike_sharing_api/app/schemas/predict.py
@@ -0,0 +1,30 @@
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+
+
+class PredictionResults(BaseModel):
+    errors: Optional[Any]
+    version: str
+    #predictions: Optional[List[int]]
+    predictions: Optional[float]
+
+class DataInputSchema(BaseModel):
+    dteday: Optional[str]
+    season: Optional[str]
+    hr: Optional[str]
+    holiday: Optional[str]
+    weekday: Optional[str]
+    workingday: Optional[str]
+    weathersit: Optional[str]
+    temp: Optional[float]
+    atemp: Optional[float]
+    hum: Optional[float]
+    windspeed: Optional[float]
+    casual: Optional[int]
+    registered: Optional[int]
+    cnt: Optional[int]
+
+
+class MultipleDataInputs(BaseModel):
+    inputs: List[DataInputSchema]
